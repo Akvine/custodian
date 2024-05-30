a15af5478b5bfa94662e603097aac9f09fc8e7a9
akvine <redgooseman55@gmail.com>
2024-05-30 09:17:06 +0300

    add handling exceptions

--- custodian-admin/src/main/java/ru/akvine/custodian/admin/controllers/rest/handler/GlobalExceptionHandler.java
+++ custodian-admin/src/main/java/ru/akvine/custodian/admin/controllers/rest/handler/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package ru.akvine.custodian.admin.controllers.rest.handler;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -7,15 +8,74 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 import ru.akvine.custodian.admin.controllers.rest.dto.common.ErrorResponse;
+import ru.akvine.custodian.core.exceptions.CommonErrorCodes;
+import ru.akvine.custodian.core.exceptions.app.AppAlreadyExistsException;
+import ru.akvine.custodian.core.exceptions.app.AppNotFoundException;
+import ru.akvine.custodian.core.exceptions.client.ClientAlreadyExistsException;
+import ru.akvine.custodian.core.exceptions.client.ClientNotFoundException;
+import ru.akvine.custodian.core.exceptions.property.PropertyAlreadyExistsException;
+import ru.akvine.custodian.core.exceptions.property.PropertyNotFoundException;
+import ru.akvine.custodian.core.exceptions.validation.ValidationException;
 
 import static ru.akvine.custodian.core.exceptions.CommonErrorCodes.GENERAL_ERROR;
 
 
 @RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler({Exception.class})
     public ResponseEntity<ErrorResponse> handleException(Exception exception) {
+        logger.error("Some error was occurred, exception = ", exception);
         ErrorResponse errorResponse = new ErrorResponse(GENERAL_ERROR, exception.getMessage(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ValidationException.class})
+    public ResponseEntity<ErrorResponse> handleValidationException(ValidationException exception) {
+        logger.info("Validation exception, message = {}", exception.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(exception.getCode(), exception.getMessage(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ClientAlreadyExistsException.class})
+    public ResponseEntity<ErrorResponse> handleClientAlreadyExistsException(ClientAlreadyExistsException exception) {
+        logger.info("Client already exists exception, message = {}", exception.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(CommonErrorCodes.CLIENT_ALREADY_EXISTS_ERROR, exception.getMessage(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({ClientNotFoundException.class})
+    public ResponseEntity<ErrorResponse> handleClientNotFoundException(ClientNotFoundException exception) {
+        logger.info("Client not found exception, message = {}", exception.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(CommonErrorCodes.CLIENT_NOT_FOUND_ERROR, exception.getMessage(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({AppAlreadyExistsException.class})
+    public ResponseEntity<ErrorResponse> handleAppAlreadyExistsException(AppAlreadyExistsException exception) {
+        logger.info("App already exists exception, message = {}", exception.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(CommonErrorCodes.APP_ALREADY_EXISTS_ERROR, exception.getMessage(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({AppNotFoundException.class})
+    public ResponseEntity<ErrorResponse> handleAppNotFoundException(AppNotFoundException exception) {
+        logger.info("App not found exception, message = {}", exception.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(CommonErrorCodes.APP_NOT_FOUND_ERROR, exception.getMessage(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({PropertyAlreadyExistsException.class})
+    public ResponseEntity<ErrorResponse> handlePropertyAlreadyExistsException(PropertyAlreadyExistsException exception) {
+        logger.info("Property already exists exception, message = {}", exception.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(CommonErrorCodes.PROPERTY_ALREADY_EXISTS_ERROR, exception.getMessage(), exception.getMessage());
+        return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler({PropertyNotFoundException.class})
+    public ResponseEntity<ErrorResponse> handlePropertyNotFoundException(PropertyNotFoundException exception) {
+        logger.info("Property not found exception, message = {}", exception.getMessage());
+        ErrorResponse errorResponse = new ErrorResponse(CommonErrorCodes.PROPERTY_NOT_FOUND_ERROR, exception.getMessage(), exception.getMessage());
         return new ResponseEntity<>(errorResponse, new HttpHeaders(), HttpStatus.BAD_REQUEST);
     }
 }
--- custodian-core/src/main/java/ru/akvine/custodian/core/exceptions/CommonErrorCodes.java
+++ custodian-core/src/main/java/ru/akvine/custodian/core/exceptions/CommonErrorCodes.java
@@ -7,6 +7,15 @@ public final class CommonErrorCodes {
 
     public static final String GENERAL_ERROR = "general.error";
 
+    public static final String CLIENT_ALREADY_EXISTS_ERROR = "client.already.exists.error";
+    public static final String CLIENT_NOT_FOUND_ERROR = "client.notFound.error";
+
+    public static final String APP_ALREADY_EXISTS_ERROR = "app.already.exists.error";
+    public static final String APP_NOT_FOUND_ERROR = "app.notFound.error";
+
+    public static final String PROPERTY_ALREADY_EXISTS_ERROR = "property.already.exists.error";
+    public static final String PROPERTY_NOT_FOUND_ERROR = "property.notFound.error";
+
     public interface Validation {
         String EMAIL_BLANK_ERROR = "email.blank.error";
         String EMAIL_INVALID_ERROR = "email.invalid.error";
